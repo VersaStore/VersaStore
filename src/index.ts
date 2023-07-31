5c475d4e49916a42b47dd13b14c1c7c748bc4edb
Stephan Meijer <me@stephanmeijer.com>
2023-07-31 15:09:50 +0200

    Include lib/

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './FileSystemInterface.js';
+export * from './file/FileSystemFile.js';
+export * from './file/content/BufferFileContent.js';
+export * from './file/content/FileContentInterface.js';
+export * from './file/content/StringFileContent.js';
+export * from './file/content/Uint8ArrayFileContent.js';
